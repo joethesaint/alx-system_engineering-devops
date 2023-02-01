4eafed63b3dc431e2fddd1460bc922adfd59d828
joethesaint <joebamisaye068@gmail.com>
2023-02-01 20:50:29 +0100

    fix cap error

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,7 +10,7 @@ def number_of_subsribers(subreddit):
     """returns the total number of subscrubbers for a given subreddit"""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
-        "User-agent": "alx-advanced api"
+        "User-Agent": "alx-advanced api"
     }
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 404:
