d38ccf94cc0e921935175c669a271ef3158b60f2
joethesaint <joebamisaye068@gmail.com>
2023-02-01 20:56:50 +0100

    fix pep8 error in t_1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -5,6 +5,7 @@ the first 10 hot posts listed for a given subreddit.
 """
 import requests
 
+
 def top_ten(subreddit):
     """
     Prints the titles of the first 10 hot posts for a given subreddit
@@ -21,8 +22,6 @@ def top_ten(subreddit):
     if response.status_code == 404:
         print("None")
         return
-    
     res = response.json().get("data")
     for child in res.get("children"):
         print(child.get("data").get("title"))
-    
