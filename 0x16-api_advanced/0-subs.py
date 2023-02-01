766ccf927567cd9615909eedfe3627a147cee595
joethesaint <joebamisaye068@gmail.com>
2023-02-01 06:00:26 +0100

    push task 0

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Quries the Reddit API and return the number of subscribers for a given subreddit"""
+import requests
+
+def number_of_subsribers(subreddit):
+    """returns the total number of subscrubbers for a given subreddit"""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        "User-agent": "alx-advanced api"
+    }
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
+        return 0
+    results = response.json().get('data')
+    return results.get('subscribers')
